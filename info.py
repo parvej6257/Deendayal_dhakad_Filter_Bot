50e9283cc21e58016c438d7a2dc7ff252600c489
parvej6257 <aditikaur1017@gmail.com>
2025-05-21 00:23:29 +0530

    Update info.py

--- info.py
+++ info.py
@@ -18,8 +18,8 @@ def is_enabled(value, default):
 # Bot Information Configuration
 # ============================
 SESSION = environ.get('SESSION', 'Deendayal_search')
-API_ID = int(environ.get('API_ID', ''))
-API_HASH = environ.get('API_HASH', '')
+API_ID = int(environ.get('API_ID', '25863055'))
+API_HASH = environ.get('API_HASH', '3d83dfe76b57b3f47de4fc8031aeb8c8')
 BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 # ============================
