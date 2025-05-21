ea66d9a80fdc97af4d7cab98c570d91d47bff30e
parvej6257 <aditikaur1017@gmail.com>
2025-05-21 09:18:58 +0530

    Update info.py

--- info.py
+++ info.py
@@ -62,7 +62,7 @@ OWNER_UPI_ID = environ.get('OWNER_UPI_ID', 'deendayaldhaked@ybl')
 # MongoDB Configuration
 # ============================
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Technical:Technical@technical.b2mml.mongodb.net/?retryWrites=true&w=majority&appName=Technical")
-DATABASE_URI2 = environ.get('DATABASE_URI2', "mongodb+srv://kauraditi101998:parvej6155@cluster0.aff18.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI2 = environ.get('DATABASE_URI2', "mongodb+srv://peji8490:parvej6155@cluster0.xp8a0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'TP_BACKUP')
 
